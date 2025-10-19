9d541ff668aa73a04bd84f4cef105c50bec8a007
Lost Again <againlost49@gmail.com>
2025-10-19 09:16:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-201821943-6d48f",
+  "appId": "1:3408064653:web:28a9c4cf04f414deeab2c4",
+  "apiKey": "AIzaSyCWYEoy9DQ3n2zYM6yPtw4Mk71hVtfpdnk",
+  "authDomain": "studio-201821943-6d48f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "3408064653"
+};
